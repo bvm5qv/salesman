d7d11755bd14f97d2299642e8a97abe7f9cb9761
Bob Hirosky <hirosky@virginia.edu>
2024-12-04 15:05:19 -0500

    add argv check

--- datareader.cpp
+++ datareader.cpp
@@ -1,3 +1,5 @@
+// example code to read in a data file of city lat,long coordinates
+
 #include <cstdio>
 #include <cstdlib>
 
@@ -30,6 +32,12 @@ int GetData(char* fname, COORD *cities){
 int main(int argc, char *argv[]){
   const int NMAX=2500;
   COORD cities[NMAX];
+
+  if (argc<2){
+    printf("Please provide a data file path as argument\n");
+    return 1;
+  }
+
   int ncity=GetData(argv[1],cities);
   printf("Read %d cities from data file\n",ncity);
   printf("Longitude  Latitude\n");
